9c3e28d79dd50b1e81e2974e6b0b8d67b68dbc77
Janek <spikedevelopment@gmail.com>
2021-04-21 15:25:23 +0200

    auth and jwt middleware

--- auth/auth.routes.config.ts
+++ auth/auth.routes.config.ts
@@ -1,18 +1,27 @@
 import { Application } from 'express'
 import { CommonRoutesConfig } from "../common/common.routes.config";
+import { body } from 'express-validator'
+import bodyValidationMiddleware from '../common/middleware/body.validation.middleware';
 
 
 export class AuthRoutes extends CommonRoutesConfig {
-
+  private _routePrefix = '/auth'
+  
   constructor(app: Application) {
     super(app, 'AuthRoutes')
   }
 
-  configureRoutes(): Application {
-    const routePrefix = '/users'
+  getRoutePrefix() {
+    return this._routePrefix
+  }
 
-    this.app.post(`${routePrefix}`, [
-      body('email').isEmail()
+  configureRoutes(): Application {
+    
+    this.app.post(`${this._routePrefix}`, [
+      body('email').isEmail(),
+      body('password').isString(),
+      bodyValidationMiddleware.verifyBodyFieldErrors,
+      authMiddl
     ])
   }
 }
